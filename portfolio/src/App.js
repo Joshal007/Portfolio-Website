cd4aa38c63e01dd94ccc05d15703903713c0de86
Joshal007 <joshaljoshi07@gmail.com>
2024-03-03 01:53:34 +0530

    completed except project section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
+import Education from './components/Education';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Education />
+      {/* <Projects /> */}
+      <Contact />
     </div>
   );
 }
--- portfolio/src/components/Education.js
+++ portfolio/src/components/Education.js
@@ -1,10 +1,74 @@
 import React from 'react'
+import '../css/education.css'
 
 const Education = () => {
     return (
-        <div>
+        <section className="qualification_section" id="education" >
+            <h2 className="section_title" >Education</h2>
+            <span className="section_subtitle" >My personel journey</span>
 
-        </div>
+            <div className="qualification_container container">
+
+
+                <div className="qualification_sections">
+                    <div className="qualification_content qualification_content-active">
+                        <div className="qualification_data">
+                            <div>
+                                <h3 className="qualification_title">B.Tech Computer Science Engineering</h3>
+                                <span className="qualification_subtitle">LNMIIT , Jaipur</span>
+
+                                <div className="qualification_calender">
+                                    <i class="uil uil-calendar-alt"></i>
+                                    2020-2024
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification_data">
+                            <div className="special"></div>
+
+
+                            <div>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                            <div style={{ margin: "0rem 1rem" }}>
+                                <h3 className="qualification_title">Senior Secondary School</h3>
+                                <span className="qualification_subtitle">New Look Senior Secondary School</span>
+
+                                <div className="qualification_calender">
+                                    <i class="uil uil-calendar-alt"></i>
+                                    2018-2020
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="qualification_data">
+
+                            <div>
+                                <h3 className="qualification_title">Secondary School</h3>
+                                <span className="qualification_subtitle">New Look Senior Secondary School</span>
+
+                                <div className="qualification_calender">
+                                    <i class="uil uil-calendar-alt"></i>
+                                    2008-2018
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
